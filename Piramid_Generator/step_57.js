4b9393d2170015885b04323bf27228d969465c76
Demegorash <felipe.solisgranados@gmail.com>
2024-06-07 18:52:15 -0600

    Exercises to 60 on the activity list, 60 tasks completed

---
+++ Piramid_Generator/step_57.js
@@ -0,0 +1,51 @@
+/*
+const character = "#";
+const count = 8;
+const rows = [];
+
+Step 57
+Because your function was no longer using the parameter, changing the value passed in the call did not affect it.
+
+Go ahead and remove the test declaration from your padRow function. Also, remove the return statement, so your function is empty again.
+
+function padRow() {
+  const test = "Testing";
+  return test;
+}
+const call = padRow();
+console.log(call);
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow() {
+  
+}
+const call = padRow();
+console.log(call);
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
---
+++ Piramid_Generator/step_58.js
@@ -0,0 +1,46 @@
+/*
+const character = "#";
+const count = 8;
+const rows = [];
+
+Step 58
+As expected, your function now returns undefined again. Your call variable is not necessary any more, so remove the call declaration and the console.log for the call variable.
+
+function padRow() {
+
+}
+const call = padRow();
+console.log(call);
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow() {
+
+}
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
