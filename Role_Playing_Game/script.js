6c0ee66e20138251624996fb36114a73491c2433
Demegorash <felipe.solisgranados@gmail.com>
2024-06-27 21:50:58 -0600

    Creating a Role Playing Game with HTML. 80 steps completed

--- Role_Playing_Game/script.js
+++ Role_Playing_Game/script.js
@@ -18,6 +18,7 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [];
 
 const locations = [
     {
@@ -73,8 +74,14 @@ function fightDragon() {
 }
 
 function buyHealth() {
-    gold += - 10;
-    health += + 10;
+    if(gold >= 10) {
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health.";
+    }
 }
 
 function buyWeapon() {
