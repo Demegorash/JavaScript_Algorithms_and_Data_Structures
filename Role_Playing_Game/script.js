ce398be2ff3a99a74df565015549306c3493d46f
Demegorash <felipe.solisgranados@gmail.com>
2024-07-04 17:39:20 -0600

    Creating a Role Playing Game with HTML. 120 steps completed

--- Role_Playing_Game/script.js
+++ Role_Playing_Game/script.js
@@ -171,11 +171,15 @@ function fightDragon() {
 }
 
 function goFight() {
-
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealthText;
 }
 
 function attack() {
-
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
 }
 
 function dodge() {
