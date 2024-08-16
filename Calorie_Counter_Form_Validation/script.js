3403c9c2355aa23a9af75ef593a8c2ab3c247889
Demegorash <felipe.solisgranados@gmail.com>
2024-08-16 15:15:11 -0600

    Creating a Calorie Counter Form Validation. 40 steps completed.

--- Calorie_Counter_Form_Validation/script.js
+++ Calorie_Counter_Form_Validation/script.js
@@ -17,4 +17,10 @@ function isInvalidInput(str) {
     return str.match(regex);
 }
 
-console.log(isInvalidInput("1e3"));
+/*  This will be in order to check if the regex is giving a null result
+console.log(isInvalidInput("10"));*/
+
+function addEntry() {
+    const targetId = "#" + entryDropdown.value;
+    const targetInputContainer = document.querySelector(targetId + " .input-container");
+}
